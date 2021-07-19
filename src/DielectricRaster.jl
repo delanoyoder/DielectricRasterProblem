dd72119134f042c6b0bb018c3e98ec49d6b18a8c
Delano Yoder <delano@Delanos-MacBook-Pro.local>
2021-07-18 22:41:33 -0500

    added Q1 function

--- src/DielectricRaster.jl
+++ src/DielectricRaster.jl
@@ -19,7 +19,44 @@ const TensorGriddedArray = GriddedArray{SMatrix{2, 2, Float64}}
 
 ## Q1
 function raster_area!(A::GriddedArray, s::Square)
-    # implement me
+
+    # Locating the lower left and upper right vertices of the Square.
+    square_ll = (s.x-s.w/2, s.y-s.w/2)
+    square_ur = (s.x+s.w/2, s.y+s.w/2)
+
+    # Storing the width, height, and area of a pixel to a variable for readability.
+    pixel_w = A.xlims[2] / (size(A.pixels)[1] - 1)
+    pixel_h = A.ylims[2] / (size(A.pixels)[2] - 1)
+    pixel_a = pixel_w * pixel_h
+
+    # Looping through the pixel array by the pixel centers.
+    for x=A.xlims[1] : pixel_w : A.xlims[2], 
+        y=A.ylims[1] : pixel_h : A.ylims[2]
+
+        # Locating the lower left and upper right vertices of the pixel.
+        pixel_ll = (x - pixel_w/2, y - pixel_h/2)
+        pixel_ur = (x + pixel_w/2, y + pixel_h/2)
+        
+        # Calculating the overlapping area of the pixel and Square.
+        ol_w = min(pixel_ur[1], square_ur[1]) - max(pixel_ll[1], square_ll[1])
+        ol_h = min(pixel_ur[2], square_ur[2]) - max(pixel_ll[2], square_ll[2])
+
+        # Calculating the current pixel indices.
+        pixel_x = Int(x / pixel_w) + 1
+        pixel_y = Int(y / pixel_h) + 1
+
+        # If the pixel width or height is negative, the area is not overlapping and should be 0.
+        if ol_w > 0 && ol_h > 0
+
+            # Calculating the fractional overlap of the Square and pixel and saving it to the respective pixel value.
+            ol_a = ol_w * ol_h
+            frac_ol =  ol_a / pixel_a
+            A.pixels[pixel_x, pixel_y] = A.pixels[pixel_x, pixel_y] * frac_ol
+        else
+            A.pixels[pixel_x, pixel_y] = 0.0
+        end
+    end
+
 end
 
 ## Q2
